8afe71b4426943ae29532b82e5441e3566c470f7
Luiz Gustavo Faria <lgfaria@gmail.com>
2013-07-06 19:26:10 +1200

    Initial Commit

---
+++ TabFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabFinder
+//
+//  Created by Luiz Gustavo Faria on 6/07/13.
+//  Copyright (c) 2013 PDM Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
